8f6282444e8f414027dde75562ad35f150e7d48c
zhijia liu <zjliu92@163.com>
2019-10-05 20:09:36 +0800

    Add naiveCpuGemm2, faster than old version.

--- gemm/cpuGemm.cpp
+++ gemm/cpuGemm.cpp
@@ -3,10 +3,6 @@
 
 using namespace std;
 
-void naiveCpuGemmFloat(void* srcA, void* srcB, void* dstC, int M, int N, int K){
-    naiveCpuGemm(static_cast<float*>(srcA), static_cast<float*>(srcB), static_cast<float*>(dstC), M, N, K);
-}
-
 template<typename T>
 void naiveCpuGemm(T* srcA, T* srcB, T* dstC, int M, int N, int K){
     for(int i = 0; i < M; i++){
@@ -18,4 +14,25 @@ void naiveCpuGemm(T* srcA, T* srcB, T* dstC, int M, int N, int K){
             dstC[i*N + j] = result;
         }
     }
+}
+
+template<typename T>
+void naiveCpuGemm2(T* srcA, T* srcB, T* dstC, int M, int N, int K){
+    for(int i = 0; i < M; i++){
+        for(int j = 0; j < N; j++){
+            dstC[i*N + j] = 0;
+        }
+    }
+    for(int k = 0; k < K; k++){
+        for(int i = 0; i < M; i++){
+            for(int j = 0; j < N; j++){
+                dstC[i*N + j] += srcA[i*K + k] * srcB[k*N + j];
+            }
+        }
+    }
+}
+
+void naiveCpuGemmFloat(void* srcA, void* srcB, void* dstC, int M, int N, int K){
+    //naiveCpuGemm(static_cast<float*>(srcA), static_cast<float*>(srcB), static_cast<float*>(dstC), M, N, K);
+    naiveCpuGemm2(static_cast<float*>(srcA), static_cast<float*>(srcB), static_cast<float*>(dstC), M, N, K);
 }
--- gemm/gemm-tester.cpp
+++ gemm/gemm-tester.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
+#include <chrono>
 
 #include "tensor.h"
 #include "gemm.h"
 
 int main(){
-    const int testSize = 16;
-    const int gemmM = testSize;
-    const int gemmN = testSize;
-    const int gemmK = testSize;
+
+    int gemmM, gemmN, gemmK;
+    std::cin >> gemmM >> gemmN >> gemmK;
+
+    float initValue;
+    int iterations;
+    bool printOutput;
+    std::cin >> initValue >> iterations >> printOutput;
     
     const int elementA = gemmM * gemmK;
     const int elementB = gemmK * gemmN;
@@ -17,19 +22,33 @@ int main(){
     Tensor tensorB(elementB, "matrixB");
     Tensor tensorC(elementC, "matrixC");
 
-    tensorA.init();
-    tensorB.init();
-    tensorC.init();
+    tensorA.init(initValue);
+    tensorB.init(initValue);
+    tensorC.init(initValue);
     
     // tensorA.print();
     // tensorB.print();
     // tensorC.print();
 
+    std::cout << "[I] Warm up... " << std::endl;
     naiveCpuGemmFloat(tensorA.getPtr(), tensorB.getPtr(), tensorC.getPtr(), gemmM, gemmN, gemmK);   
+    
+    std::cout << "[I] Run kernel with iteration " << iterations << "..." << std::endl;
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0 ; i < iterations; i++){
+        naiveCpuGemmFloat(tensorA.getPtr(), tensorB.getPtr(), tensorC.getPtr(), gemmM, gemmN, gemmK);   
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration<double>(end - start); 
+    
+    std::cout << "Time: " << duration.count() / iterations << std::endl;
 
-    // tensorA.print();
-    // tensorB.print();
-    // tensorC.print();
+    if(printOutput){
+        tensorA.print();
+        tensorB.print();
+        tensorC.print();
+    }
+
 
     return 0;
 }
